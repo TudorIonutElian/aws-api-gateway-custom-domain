b0bed39096991f07fd341dd5301b512bc38120df
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2025-03-15 22:13:51 -0700

    LRN-- set initial code

---
+++ apigateway.tf
@@ -0,0 +1,73 @@
+/**********************************************************
+  # Add the API Gatewy
+**********************************************************/
+
+resource "aws_api_gateway_rest_api" "youtube_demo_api" {
+  name        = "Youtube Demo API"
+  description = "This is a demo API for the Youtube demo"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+/**********************************************************
+*** # Add /demo resource to the API Gateway
+**********************************************************/
+resource "aws_api_gateway_resource" "youtube_demo_resource" {
+  rest_api_id = aws_api_gateway_rest_api.youtube_demo_api.id
+  parent_id   = aws_api_gateway_rest_api.youtube_demo_api.root_resource_id
+  path_part   = "youtube-demo"
+}
+
+
+/**********************************************************
+*** # Add first gateway METHOD - aws_api_gateway_method
+**********************************************************/
+resource "aws_api_gateway_method" "proxy_aws_api_gateway_method" {
+  rest_api_id   = aws_api_gateway_rest_api.cloudwatch_mock_api.id
+  resource_id   = aws_api_gateway_resource.demo_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+
+resource "aws_api_gateway_method_response" "proxy_aws_api_gateway_method_response_demo" {
+  rest_api_id = aws_api_gateway_rest_api.youtube_demo_api.id
+  resource_id = aws_api_gateway_resource.youtube_demo_resource.id
+  http_method = aws_api_gateway_method.proxy_aws_api_gateway_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.youtube_demo_api.id
+  resource_id = aws_api_gateway_resource.youtube_demo_resource.id
+  http_method = aws_api_gateway_method.proxy_aws_api_gateway_method.http_method
+  status_code = aws_api_gateway_method_response.proxy_aws_api_gateway_method_response_demo.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [
+    aws_api_gateway_method.proxy_aws_api_gateway_method,
+    aws_api_gateway_integration.lambda_integration_write_payload_func
+  ]
+}
+
+
+/**********************************************************
+*** # API Deployment
+**********************************************************/
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.youtube_demo_api.id
+  stage_name  = "v1"
+}
---
+++ route53.tf
@@ -0,0 +1,31 @@
+/*****************************************************
+ * Data source to get the hosted zone ID
+ ****************************************************/
+data "aws_route53_zone" "learndevtech" {
+  name         = "learndevtech.com"
+  private_zone = false
+}
+
+/*****************************************************
+ * Create a certificate for the domain
+ ****************************************************/
+resource "aws_acm_certificate" "learndevtech_certificate" {
+  domain_name       = "cloud-watch.learndevtech.com"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+/*****************************************************
+ * Create a record for the API Gateway
+ ****************************************************/
+resource "aws_route53_record" "api_domain_record" {
+  name = "api"
+  type = "CNAME"
+  ttl  = "300"
+
+  records = ["${aws_api_gateway_rest_api.cloudwatch_mock_api.id}.execute-api.eu-central-1.amazonaws.com"]
+  zone_id = data.aws_route53_zone.learndevtech.zone_id
+}
