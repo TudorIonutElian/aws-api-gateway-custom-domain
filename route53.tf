dc098a65fbdc78fc91b681aa11295993926a98d6
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2025-03-18 11:58:24 +0200

    Add domain mapping

--- route53.tf
+++ route53.tf
@@ -6,6 +6,17 @@ data "aws_route53_zone" "learndevtech" {
   private_zone = false
 }
 
+resource "aws_api_gateway_domain_name" "custom_domain" {
+  domain_name = "api.learndevtech.com"
+  certificate_arn = aws_acm_certificate.api_certificate.arn
+}
+
+resource "aws_api_gateway_base_path_mapping" "youtube_api_mapping" {
+  api_id      = aws_api_gateway_rest_api.youtube_demo_api.id
+  stage_name  = aws_api_gateway_stage.youtube_demo_stage.stage_name
+  domain_name = aws_api_gateway_domain_name.custom_domain.domain_name
+}
+
 /*****************************************************
  * Create a record for the API Gateway
  ****************************************************/
@@ -14,6 +25,11 @@ resource "aws_route53_record" "api_domain_record" {
   type = "CNAME"
   ttl  = "300"
 
-  records = ["${aws_api_gateway_rest_api.youtube_demo_api.id}.execute-api.eu-central-1.amazonaws.com"]
+  records = ["api.learndevtech.com"]
   zone_id = data.aws_route53_zone.learndevtech.zone_id
+}
+
+resource "aws_acm_certificate" "api_certificate" {
+  domain_name       = "api.learndevtech.com"
+  validation_method = "DNS"
 }
