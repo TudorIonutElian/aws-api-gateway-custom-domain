0487d95bc2065747a13cac2f89dd15e6d3a9f218
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2025-03-18 11:50:31 +0200

    LRN -- update the mock integration

--- apigateway.tf
+++ apigateway.tf
@@ -66,3 +66,17 @@ resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.youtube_demo_api.id
   stage_name  = "v1"
 }
+
+/**********************************************************
+*** # Add lambda integration
+**********************************************************/
+resource "aws_api_gateway_integration" "lambda_integration_get_logs_func" {
+  rest_api_id             = aws_api_gateway_rest_api.youtube_demo_api.id
+  resource_id             = aws_api_gateway_resource.youtube_demo_resource.id
+  http_method             = aws_api_gateway_method.proxy_aws_api_gateway_method.http_method
+  integration_http_method = "POST"
+  type                    = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
